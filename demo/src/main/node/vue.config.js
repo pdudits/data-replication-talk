f94203adef06c85b49bbc028875e614ec54b42cc
Patrik Dudits <patrik.dudits@payara.fish>
2019-05-25 22:14:51 +0200

    Setup proxy for vue app

---
+++ demo/src/main/node/vue.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+    devServer: {
+        port: 8082,
+        proxy: {
+            // plain string didn't work actually
+            ".+": {
+                target: 'http://localhost:8080/'
+            }
+        }
+    }
+}
